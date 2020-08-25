5fb2875c1af184aba4e1972f53cdebae3d58db99
Eric Jiang <ericjiang.yang@gmail.com>
2020-08-25 17:05:52 -0400

    data file size

---
+++ database_files_size.sql
@@ -0,0 +1,20 @@
+with fs as (
+    select database_id,
+        type,
+        size * 8.0 / 1024 size
+    from sys.master_files
+)
+select name,
+    (
+        select sum(size)
+        from fs
+        where type = 0
+            and fs.database_id = db.database_id
+    ) DataFileSizeInMB,
+    (
+        select sum(size)
+        from fs
+        where type = 1
+            and fs.database_id = db.database_id
+    ) LogFileSizeInMB
+from sys.databases db
